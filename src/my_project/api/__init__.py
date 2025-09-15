a87bccc6b7e7376eb84943cd937c45f943da1e7f
阳光很暖 <69832745+JiangJiangYo@users.noreply.github.com>
2025-09-15 11:13:40 +0800

    chore: 集成用户API路由

--- src/my_project/api/__init__.py
+++ src/my_project/api/__init__.py
@@ -1,7 +1,5 @@
 from fastapi import APIRouter
+from .user import router as user_router
 
 router = APIRouter()
-
-@router.get("/")
-async def root():
-    return {"msg": "Welcome to MyFastAPI 企业级后台"}
+router.include_router(user_router)
