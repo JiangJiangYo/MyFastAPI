85b64483085f573bf8aa32b8f3db1a03d731563d
阳光很暖 <69832745+JiangJiangYo@users.noreply.github.com>
2025-09-15 11:13:45 +0800

    chore: 集成API主路由

--- src/my_project/main.py
+++ src/my_project/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
+from .api import router as api_router
 from .config import settings
 
 app = FastAPI(title="MyFastAPI 企业级后台管理系统")
-
-@app.get("/")
-def read_root():
-    return {"msg": "Welcome to MyFastAPI!"}
+app.include_router(api_router)
